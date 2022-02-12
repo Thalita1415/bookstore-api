dbad4dd39fddb479079a85a78ad5d91776149250
Thalita Gonçalves <Thalita.freitas1415@hotmail.com>
2022-02-11 22:59:17 -0300

    repositories implementadas

--- src/main/java/com/thalita/bookstore/domain/Livro.java
+++ src/main/java/com/thalita/bookstore/domain/Livro.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+
 
 @Entity
 public class Livro  implements Serializable {
@@ -24,7 +28,9 @@ public class Livro  implements Serializable {
 	private String titulo;
 	private String nome_autor;
 	private String texto;
-
+	
+	@ManyToOne
+	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 
 	public Livro() {
---
+++ src/main/java/com/thalita/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.thalita.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thalita.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository  extends JpaRepository<Categoria,Integer>{
+
+}
